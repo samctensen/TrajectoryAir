ce2b91ae1aba34da2f22da423988d468787bc065
Sam Christensen <samctensen@gmail.com>
2024-05-17 14:26:37 -0600

    drizzle setup

---
+++ drizzle/schema.ts
@@ -0,0 +1,23 @@
+import {
+  pgTable,
+  serial,
+  text,
+  timestamp,
+  uniqueIndex,
+} from 'drizzle-orm/pg-core';
+ 
+export const UsersTable = pgTable(
+  'users',
+  {
+    id: serial('id').primaryKey(),
+    name: text('name').notNull(),
+    email: text('email').notNull(),
+    image: text('image').notNull(),
+    createdAt: timestamp('createdAt').defaultNow().notNull(),
+  },
+  (users) => {
+    return {
+      uniqueIdx: uniqueIndex('unique_idx').on(users.email),
+    };
+  },
+);
