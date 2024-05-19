dce50138b0ba02f4b859cb6fb925106cf2938747
Sam Christensen <samctensen@gmail.com>
2024-05-18 18:49:50 -0600

    supabase setup

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,12 @@
-import '@/drizzle/envConfig';
+import * as dotenv from 'dotenv';
 import { defineConfig } from 'drizzle-kit';
  
+dotenv.config({ path: '/env'});
+
 export default defineConfig({
   schema: './drizzle/schema.ts',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.POSTGRES_URL!,
+    url: process.env.SUPABASE_URL ||  '',
   },
 });
