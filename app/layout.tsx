c6dc43fc17423dfaf69a80104b0c13610fa9484e
Sam Christensen <samctensen@gmail.com>
2024-05-29 15:34:06 -0600

    updated tab icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Trajectory Air",
   description: "Trajectory Air",
+  icons: {
+    icon: "/favicon.ico",
+  }
 };
 
 export default function RootLayout({
